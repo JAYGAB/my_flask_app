eb1fc8310591a5384c33524609f85b44f07bc34f
JayGab Santos <jaygabriel.santos15@gmail.com>
2024-10-06 16:26:18 +0800

    Initial commit of Flask API and model

--- lib/onboarding/color.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Define the gradient colors
-var GradientBackground = LinearGradient(
-  colors: [Colors.transparent, Colors.black.withOpacity(0.6)], 
-  begin: Alignment.topLeft,
-  end: Alignment.bottomRight,
-);
